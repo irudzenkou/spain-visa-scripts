e337c7578553cf33642c913e077c4e7b35ee818b
irudzenkou <86909432+irudzenkou@users.noreply.github.com>
2024-02-12 22:29:32 +0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,6 +1,3 @@
 (function() {
-     var script = document.createElement('script')
-     script.setAttribute("type", "text/javascript");
-     script.setAttribute("src", "https://raw.githubusercontent.com/irudzenkou/spain-visa-scripts/main/jquery-1.11.0.min.js");
-     document.getElementsByTagName("head")[0].appendChild(script);
+     alert("alert");
 })(); 
